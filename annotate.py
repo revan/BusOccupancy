35f9c96fed90fe0c7f0135808c0c966c6bf15ef4
Daniel Bordak <dbordak@gmail.com>
2014-04-19 17:47:27 -0400

    Made annotations a lot nicer

--- annotate.py
+++ annotate.py
@@ -1,11 +1,16 @@
 #Adds (time, label) pairs from file to plot
-def annotate(plot, hist, file, labely=0):
-	if not labely:
-		labely = max(hist)
-	for line in file:
-		(x, label) = line.split(' ', 1)
-		x = int(x)
-		y = hist[x]
+def annotate(plot, hist, file, div):
+    #if not labely:
+    #    labely = max(hist)
+    for line in file:
+        (x, label) = line.split(' ', 1)
+        x = int(x)
+        label = label.rstrip()
+        y = hist[x/div]
 
-		plot.annotate(label, xy=(x, y), xytext=(x, labely), arrowprops=dict(facecolor='red', shrink=0.05))
-
+        plot.annotate(label, xy=(x, y), xytext=(50, 60),
+                      textcoords="offset points",
+                      bbox=dict(boxstyle="round", fc="#d3d3d3", ec="#a8a8a8"),
+                      arrowprops=dict(arrowstyle="fancy", fc="#d3d3d3",
+                                      facecolor='purple', ec="#a8a8a8",
+                                      connectionstyle="angle3,angleA=0,angleB=-90"))
