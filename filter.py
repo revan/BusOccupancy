6f119b3f665f50f7454392f34fde0fb4090d0f47
Daniel Bordak <dbordak@gmail.com>
2014-04-24 16:32:10 -0400

    Fixed filter removing during iteration

--- filter.py
+++ filter.py
@@ -1,23 +1,39 @@
 def filter(jason, routers=False, strength=False, strlim=0,
            removeEmptyStr=False):
+    routers = set(jason["routers"])
+    removeArr = []
     for packet in jason["packets"]:
+        removePkt = False
         if routers:
-            filterRouters(packet, jason["packets"], jason["routers"])
+            removePkt = filterRouters(packet, routers)
         if strength:
-            filterStrength(packet, jason["packets"], strlim, removeEmptyStr)
+            removePkt = removePkt or filterStrength(packet, strlim,
+                                                    removeEmptyStr)
+        if removePkt:
+            removeArr.append(packet)
+    for packet in removeArr:
+        jason["packets"].remove(packet)
     if routers:
         del jason["routers"]
 
-def filterRouters(packet, packets, routers):
-    for type, add in packet["adds"].item():
+# Removes router addresses from each packet
+# Returns True if a packet has no addresses attached to it
+def filterRouters(packet, routers):
+    remove=[]
+    for type, add in packet["adds"].items():
         if add in routers:
-            del packet[type]
+            remove.append(type)
+    for type in remove:
+        del packet["adds"][type]
     if len(packet["adds"]) == 0:
-        del packets.remove(packet)
+        return True
+    return False
 
-def filterStrength(packet, packets, str, removeEmpty=False):
+# Returns True if a packet doesn't meet the strength limit
+def filterStrength(packet, str, removeEmpty=False):
     if "str" in packet:
         if packet["str"] < str:
-            packets.remove(packet)
+            return True
     elif removeEmpty:
-        packets.remove(packet)
+        return True
+    return False
