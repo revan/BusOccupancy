9917248939d167ab11914e1317ea3c2d3a7fe0c4
Daniel Bordak <dbordak@gmail.com>
2014-05-06 17:52:16 -0400

    Switched Packethist to a list comprehension

--- packethist.py
+++ packethist.py
@@ -12,23 +12,19 @@ def addPacket(packet, addresses):
             addresses[add][2] += 1
 
 def plotPacketHistogram(jason):
-    packetAxis = []
     macAddresses = {}
-
     jason["packets"].apply(lambda row: addPacket(row, macAddresses), axis=1)
 
     last = jason["packets"]["time"].iget(-1)
 
-    for value in macAddresses.values():
-        diff = value[0]+last-value[1]
-
-        if diff < 120:
-            packetAxis.append(value[2])
+    packetAxis = [val[2] for val in macAddresses.values()
+                  if val[0]+last-val[1] < 120]
 
     xmax = len(packetAxis)
     plot.xlim(0,xmax)
     plot.yscale('log')
-    plot.bar(range(xmax), sorted(packetAxis), color="black", align="center",log=True)
+    plot.bar(range(xmax), sorted(packetAxis), color="black",
+             align="center",log=True)
     plot.xlabel("Unique MAC addresses present for the entire class period")
     plot.ylabel('Number of Packets seen for each MAC address')
     plot.title('April 7th, Classroom Packet Distribution')
