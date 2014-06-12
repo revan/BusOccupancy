ae2c8d5d278f1416ced9fe3523e2233a90c49699
Erin Corrado <e.corrado144@gmail.com>
2014-06-11 20:00:19 -0400

    created images for bus route. Updated labels to include number codes (first stop is Buell now). Number codes are shown on the overhead images

--- graphlib.py
+++ graphlib.py
@@ -9,7 +9,7 @@ def annotate(label, x, y, xoff=0, yoff=0):
     plot.annotate(label, xy=(x, y), xytext=(xoff, yoff),
                   textcoords="offset points",
                   bbox=dict(boxstyle="round", fc="#d3d3d3", ec="#a8a8a8"),
-                  arrowprops=dict(arrowstyle="fancy", fc="#d3d3d3",
+                  arrowprops=dict(arrowstyle="->", fc="#d3d3d3",
                                   ec="#a8a8a8", connectionstyle=
                                   "angle3,angleA=0,angleB=-90"))
 
@@ -21,7 +21,7 @@ def wideAnnotate(plot, hist, binsize, ymax=10000, file='data/sched.json'):
     for stop in reversed(jason):
         x = stop["start"]
         y = hist[int(x)/binsize]
-        annotate(stop["name"], x, y, 15, 25 if abs(yprev-y) < ymax/20 else 15)
+        annotate(stop["code"], x, y, 15, 25 if abs(yprev-y) < ymax/20 else 15)
         yprev = y
 
 
@@ -32,7 +32,7 @@ def squareAnnotate(plot, file='data/sched.json', ymax=0):
         x = stop["start"]
         plot.plot([0, x], [x, x], 'purple', linewidth=1)
         plot.plot([x, x], [x, ymax], 'purple', linewidth=1)
-        annotate(stop["name"], x, x, 10, -10)
+        annotate(stop["code"], x, x, 10, -10)
 
 
 def makePlot(x, y, prefix, type):
@@ -42,14 +42,14 @@ def makePlot(x, y, prefix, type):
 
 
 def makeWidePlot(prefix, type):
-    plot.gcf().set_size_inches(6.9, 3.8)
+    plot.gcf().set_size_inches(3.4, 3.8)
     plot.tight_layout()
-    plot.savefig('img/' + prefix + '-' + type + '-wide.pgf',
+    plot.savefig('img/' + prefix + '-' + type + '-wide.png',
                  bbox_inches='tight')
 
 
 def makeSquarePlot(prefix, type):
-    plot.gcf().set_size_inches(6.9, 6.5)
+    plot.gcf().set_size_inches(3.4, 6.5)
     plot.tight_layout()
     plot.savefig('img/' + prefix + '-' + type + '-square.pgf',
                  bbox_inches='tight')
